df19b74876cecb6f42f4e8b2bd7ec46424d07d7c
Krimboucha <hamza.berraho.ma@example.com>
2023-08-09 00:28:02 +0100

    task 1

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,9 +3,9 @@ import random
 
 number = random.randint(-10000, 10000)
 last_digit = abs(number) % 10
-if last_digit > 5:
+if (abs(number) % 10) > 5:
     rest = "greater than 5"
-elif last_digit == 0:
+elif (abs(number) %10 ) == 0:
     rest = "0"
 else:
     rest = "less than 6 and not 0"
