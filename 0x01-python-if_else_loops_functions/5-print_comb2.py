49f9746d2a81d408b7deb2f2ee9ac2ceaffa02db
Krimboucha <hamza.berraho.ma@example.com>
2023-08-09 01:04:45 +0100

    task 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -2,8 +2,8 @@
 for i in range(100):
     sep = ", "
     if i < 10:
-        print("0{:d}".format(i), end=sep
-        continue)
+        print("0{:d}".format(i), end=sep)
+        continue
     if i == 99:
         sep = "\n"
     print("{:d}".format(i), end=sep)
