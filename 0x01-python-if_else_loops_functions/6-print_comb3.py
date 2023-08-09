cdd9b95f2c4054d69b39379f308487e110299858
Krimboucha <hamza.berraho.ma@example.com>
2023-08-09 02:48:16 +0100

    task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,7 +2,8 @@
 sep = ", "
 for i in range(10):
     for j in range(i + 1, 10):
-        print("{}{}".format(i, j), end=sep)
+#        print("{}{}".format(i, j), end=sep)
         if i == 8 and j == 9:
             sep = "\n"
-            print("{}{}".format(i, j), end=sep)
+        print("{}{}".format(i, j), end=sep)
+#           print("{}{}".format(i, j), end=sep)
