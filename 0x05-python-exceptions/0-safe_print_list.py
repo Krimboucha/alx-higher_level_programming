50ae2e4903db601bfb79c16791d0833cad945cb3
Krimboucha <hamza.berraho.ma@example.com>
2023-08-28 21:27:53 +0100

    task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
         c = 0
         for i in my_list:
             if c < x:
-                print(i, end='')
+                print(f"{i}", end='')
                 c += 1
         print()
         return c
