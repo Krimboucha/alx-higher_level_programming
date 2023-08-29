3fd6d18a2ac9a0968e5abe231a237683de460e98
Krimboucha <hamza.berraho.ma@example.com>
2023-08-29 21:36:11 +0100

    task 2 n 3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -4,11 +4,14 @@
 
 class Square:
     """square with private size attribute
-    size is an int
-    """
+     and public method area"""
     def __init__(self, size=0):
         self.__size = size
         if type(size) is not int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
+
+    def area(self):
+        area = self.__size ** 2
+        return area
