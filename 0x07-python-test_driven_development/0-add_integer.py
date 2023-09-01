31a4a8e1f59e0a3aa21e2ceac0536183d53a12c0
Krimboucha <hamza.berraho.ma@example.com>
2023-09-01 21:41:31 +0100

    real 0/2

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+"""
+    add_integer adds two ints or floats
+    Parameters: a int or float
+                b int or float (default value 98)
+"""
 def add_integer(a, b=98):
+    """
+    Returns: int: The sum of a and b.
+    """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
