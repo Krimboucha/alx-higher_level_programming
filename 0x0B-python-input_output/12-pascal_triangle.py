b0859370f7eaa7814678ac80b6c0b492bc26fe3b
Krimboucha <hamza.berraho.ma@example.com>
2023-09-13 03:30:43 +0100

    lemme sleep

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -3,12 +3,11 @@
 
 
 def pascal_triangle(n):
-    """Represent Pascal's Triangle of size n.
+    """Represent Pascal's Triangle of size n
     Returns a list of lists of integers representing the triangle.
     """
     if n <= 0:
         return []
-
     triangles = [[1]]
     while len(triangles) != n:
         tri = triangles[-1]
