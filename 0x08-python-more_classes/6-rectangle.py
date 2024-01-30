e5037f20449951f805611a0bcd4fd9dedcad61b6
Krimboucha <hamza.berraho.ma@example.com>
2024-01-31 00:41:22 +0100

    task 6

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
-"""Rectangle Module
+"""Rctangle module
 """
 
 
 class Rectangle:
     """Defines a rectangle
     """
+    number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         """Initializes an instance of the
         Rectangle class
         """
         self.width = width
         self.height = height
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
