f076664d24008137c3709eff8a02fd4e025c6f8b
Krimboucha <hamza.berraho.ma@example.com>
2024-01-31 00:57:01 +0100

    task 7

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -72,7 +72,7 @@ class Rectangle:
         if not self.__width or not self.__height:
             return ""
         return ((str(self.print_symbol) *
-            self.__width + "\n") * self.__height)[:-1]
+                self.__width + "\n") * self.__height)[:-1]
 
     def __repr__(self):
         """Returns a string representation of
