f506f1c0046049e192dc49276bdeb02f5d2d197d
Krimboucha <hamza.berraho.ma@example.com>
2024-02-05 22:27:59 +0100

    Yes I can

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -3,9 +3,9 @@
 """
 
 def add_attribute(obj, attr_name, attr_value):
-    """Adds a new attribute to an object if possible"""
+    """Adds a new attribute to an object if possible
+    """
     if hasattr(obj, '__dict__'):
         obj.__dict__[attr_name] = attr_value
     else:
         raise TypeError("can't add new attribute")
-
