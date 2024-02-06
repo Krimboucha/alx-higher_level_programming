21a5888badcde5e1d44691edb0c7178f37fb89d4
Krimboucha <hamza.berraho.ma@example.com>
2024-02-06 16:45:23 +0100

    task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,9 +4,6 @@
 
 
 def read_file(filename=""):
-    try:
-        with open(filename, 'r', encoding='utf-8') as file:
-            read_data = file.read()
-            print(read_data)
-    except Exception as e:
-        print(f"Error reading the file: {e}")
+    with open(filename, 'r', encoding='utf-8') as file:
+        read_data = file.read()
+        print(read_data)
