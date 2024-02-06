ebc971d18c16abf537baa2375c9a2a3322f62d18
Krimboucha <hamza.berraho.ma@example.com>
2024-02-06 19:13:25 +0100

    task 10

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -13,7 +13,7 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-    def to_json(self, attrs = None):
+    def to_json(self, attrs=None):
         """ retrieves the dictionary
         representation of a Student
         instance
