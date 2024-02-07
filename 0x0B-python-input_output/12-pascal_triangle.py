da0f09170b5d5a3aeb265dcb94bc156f278521e1
Krimboucha <hamza.berraho.ma@example.com>
2024-02-07 01:13:51 +0100

    task12

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -4,21 +4,17 @@
 
 
 def pascal_triangle(n):
+    """ returns a list of lists of integers
+    representing the Pascal’s triangle of n
+    """
     if n <= 0:
         return []
-
     triangle = []
-
     for i in range(n):
-        row = [1]  # First element in every row is 1
-
-        # Calculate the next elements in the row based on the previous row
+        row = [1]
         for j in range(1, i):
             row.append(triangle[i-1][j-1] + triangle[i-1][j])
-
         if i > 0:
-            row.append(1)  # Last element in every row is 1
-
+            row.append(1)
         triangle.append(row)
-
     return triangle
